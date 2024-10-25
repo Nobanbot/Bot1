06f57b2a114cec789c683445e6b35a71239f1f64
Nobanbot <bot615483@gmail.com>
2024-10-26 03:27:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/soul/soul.py",
+    "postStartCommand": "python3 /workspaces/Bot1/daksh.py",
     "customizations": {
         "vscode": {
             "settings": {
